6b4a1d1d5f236c93a28fe524dc6879a3c615f276
kiefer6451 <ekiefer@uwo.ca>
2020-04-21 13:19:02 -0400

    Scrolling update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $('.nav-link').on('click', () => {
     $('#navbarTogglerDemo02').collapse('hide'); 
 });
+
+$("nav").find("a").click(e => {
+    const section = e.target.href.split('#')[1];
+    if (section == 'about-section' || section == 'projects-section'){
+        e.preventDefault(); 
+        $("html, body").scrollTop($(`#${section}`).offset().top - $('.navbar').get(0).clientHeight); 
+    }   
+});
