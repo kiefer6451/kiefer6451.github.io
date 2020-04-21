3d711d2b594b577850688ac4e01edefd55fd0e07
kiefer6451 <ekiefer@uwo.ca>
2020-04-21 14:16:07 -0400

    Loading screen update

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ $('.loading-icon').css({
     top: `${$('.navbar').get(0).clientHeight}px`
 }); 
 
-$(window).ready(() => {
-    $(".loading-icon").hide();
+$(window).on('load', () => {
+    $('.loading-icon').hide();
 });
 
 $('.nav-link').on('click', () => {
     $('#navbarTogglerDemo02').collapse('hide'); 
 });
 
-$("nav").find("a").click(e => {
+$('nav').find('a').click(e => {
     const section = e.target.href.split('#')[1];
     if (section == 'about-section' || section == 'projects-section'){
         e.preventDefault(); 
-        $("html, body").scrollTop($(`#${section}`).offset().top - $('.navbar').get(0).clientHeight); 
+        $('html, body').scrollTop($(`#${section}`).offset().top - $('.navbar').get(0).clientHeight); 
     }   
 });
 
