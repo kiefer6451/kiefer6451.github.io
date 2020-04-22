ad762c945bc7ca40995400d919c906b0ea09f6d4
kiefer6451 <ekiefer@uwo.ca>
2020-04-22 09:05:45 -0400

    Scrolling fix

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $('nav').find('a').click(e => {
     const section = e.target.href.split('#')[1];
     if (section == 'about-section' || section == 'projects-section'){
         e.preventDefault(); 
-        $('html, body').scrollTop($(`#${section}`).offset().top - $('.navbar').get(0).clientHeight); 
+        $('html, body').scrollTop($(`#${section}`).offset().top - $('.navbar').height()); 
     }   
 });
 
